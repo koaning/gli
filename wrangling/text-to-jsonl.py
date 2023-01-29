a4ed82e62c7bba0cf520b03a1b1d327e866d6169
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-01-29 12:48:54 +0000

    text2jsonl

---
+++ wrangling/text-to-jsonl.py
@@ -0,0 +1,27 @@
+import itertools as it 
+
+import typer
+import srsly
+import json
+from pathlib import Path
+
+
+def text2jsonl(txt_path: Path = typer.Argument(..., help="A .txt file"),
+               out_path: Path = typer.Option(None, help="Output file. Will print if not provided."),
+               n: int = typer.Option(None, help="Only consider top `n` texts.")):
+    """This prints your name"""
+
+    with open(txt_path, "r") as f:
+        lines = f.readlines()
+        
+    g = ({"text": text.replace("\n", "")} for text in lines)
+    if n:
+        g = it.islice(g, n)
+    if out_path:
+        srsly.write_jsonl(out_path, g)
+    else:
+        for ex in g:
+            print(json.dumps(ex))
+
+if __name__ == "__main__":
+    typer.run(text2jsonl)
